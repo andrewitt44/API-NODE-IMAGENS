28b7ecc498bb3289d58313a01ea85f49b25d2ffb
André Felipe Witt <andreflipewitt@gmail.com>
2025-03-04 20:56:02 -0300

    Alterações feitas no User

--- src/Entity/Usuario.js
+++ src/Entity/Usuario.js
@@ -2,8 +2,6 @@ class Usuario {
     constructor(nome) {
         this.nome = nome;
         
-        const date = new Date();
-        this.data_criacao = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;;
     }
 }
 
